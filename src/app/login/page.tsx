2b296f170f1f2330ffbcf49490d56779813dff4d
Siddhant Jain <codebysidd@gmail.com>
2025-04-24 13:14:25 +0000

    same metadata added to login page as it acts like home page for users

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,6 +2,38 @@ import Features from "@/components/Features";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import HowToUse from "@/components/HowToUse";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Note Mind",
+    metadataBase: new URL('https://note-mind.vercel.app'),
+    icons: {
+        icon: "/site-icon.png",
+    },
+    keywords: ["Siddhant Jain", "note mind", "ai todo app"],
+    description: "Note Mind is an AI powered task management app",
+    openGraph: {
+        title: "Note Mind",
+        description: "Note Mind is an AI powered task management app",
+        url: "/",
+        siteName: "Note Mind",
+        type: "website",
+        locale: "en_US",
+        images: [
+            {
+                url: "/banner.png",
+                width: 800,
+                height: 600,
+            }
+        ]
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: "Note Mind",
+        description: "Note Mind is an AI powered task management app",
+        images: ["/banner.png"]
+    },
+};
 
 export default function page() {
     return (
